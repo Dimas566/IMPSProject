b64b3c8b3ce0fbabb4a6b91876a8e9e7996fc125
Dimas <dimas.carabantes@usonsonate.edu.sv>
2023-09-30 09:32:07 -0600

    Mantenimiento de estudiantes

--- src/repositories/EstudianteRepository.js
+++ src/repositories/EstudianteRepository.js
@@ -10,6 +10,16 @@ module.exports = {
         } catch (error) {
             console.error('Ocurrio un problema al consultar la lista de estudiantes: ', error);
         }
+    },
+
+    // Eliminar un estudiante
+    eliminarEstudiante: async(idestudiante) => {
+        try{
+          const result = await pool.query('DELETE FROM estudiantes WHERE idestudiante = ?', [idestudiante]);
+          return result.affectedRows > 0;
+        }catch(error){
+          console.error('Erro al eliminar el registro', error);
+        }
     }
 }
 

d1e08d7581168342573b157a3fb4235464607a19
Dimas <dimas.carabantes@usonsonate.edu.sv>
2023-11-04 09:36:38 -0600

    Se agrega inicio de sesion y proteccion de rutas

--- src/routes/estudiantes.js
+++ src/routes/estudiantes.js
@@ -2,32 +2,26 @@ const express = require('express');
 const router = express.Router();
 const queries = require('../repositories/EstudianteRepository');
 const carrerasQuery = require('../repositories/CarreraRepository');
-const Swal = require('sweetalert2');
+const { isLoggedIn } = require('../lib/auth');
 
 // Endpoint para mostrar todos los estudiantes
-router.get('/', async (request, response) => {
+router.get('/', isLoggedIn, async (request, response) => {
     const estudiantes = await queries.obtenerTodosLosEstudiantes();
 
      response.render('estudiantes/listado', {estudiantes}); // Mostramos el listado de estudiantes
 });
 
 // Endpoint que permite mostrar el formulario para agregar un nuevo estudiante
-router.get('/agregar', async(request, response) => {
+router.get('/agregar', isLoggedIn, async(request, response) => {
    
-   /* const lstCarreras = await carrerasQuery.obtenerTodosLasCarreras();
+    const lstCarreras = await carrerasQuery.obtenerTodosLasCarreras();
 
     // Renderizamos el formulario
-    response.render('estudiantes/agregar', {lstCarreras});*/
-        Swal.fire({
-        title: 'Hola',
-        text: 'Este es un ejemplo de SweetAlert en Handlebars.',
-        icon: 'success',
-        confirmButtonText: '¡Entendido!'
-    });
+    response.render('estudiantes/agregar', {lstCarreras});
 });
 
 // Endpoint para agregar un estudiante
-router.post('/agregar', async(request, response) => {
+router.post('/agregar', isLoggedIn, async(request, response) => {
     const { accion, idestudiante, nombre,apellido, email, idcarrera, usuario } = request.body;
     const nuevoEstudiante = { idestudiante, nombre, apellido, email, idcarrera, usuario };
 
@@ -39,7 +33,7 @@ router.post('/agregar', async(request, response) => {
 
 
 // Endpoint que permite mostrar el formulario para modificar un estudiante
-router.get('/modificar/:idestudiante', async(request, response) => {
+router.get('/modificar/:idestudiante', isLoggedIn, async(request, response) => {
     const {idestudiante} = request.params; 
     const lstCarreras = await carrerasQuery.obtenerTodosLasCarreras();
 
@@ -50,7 +44,7 @@ router.get('/modificar/:idestudiante', async(request, response) => {
 });
 
 // Endpoint que permite actualizar un estudiante
-router.post('/modificar/:id', async(request, response) => {
+router.post('/modificar/:id', isLoggedIn, async(request, response) => {
     const {id} = request.params; 
     const {  idestudiante, nombre,apellido, email, idcarrera, usuario } = request.body;
     const nuevoEstudiante = { idestudiante, nombre, apellido, email, idcarrera, usuario };
@@ -62,7 +56,7 @@ router.post('/modificar/:id', async(request, response) => {
 });
 
 // Endpoint que permite eliminar un estudiante
-router.get('/eliminar/:idestudiante', async(request, response) => {
+router.get('/eliminar/:idestudiante', isLoggedIn, async(request, response) => {
     // Desestructuramos el objeto que nos mandan en la peticion y extraemos el idestudiante
     const { idestudiante } = request.params;
     const resultado = await queries.eliminarEstudiante(idestudiante);
